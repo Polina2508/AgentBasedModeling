c0843ea6647b3cc41844d97d32638af052ea4419
Polina <ptomasevskaa30@gmail.com>
2021-03-17 21:40:46 +0300

    commit

--- model.py
+++ model.py
@@ -1,46 +1,86 @@
 from mesa import Model, Agent
-from mesa.space import SingleGrid
 from mesa.time import RandomActivation
+from mesa.space import SingleGrid
+from mesa.datacollection import DataCollector
 
 
-class Scientist(Agent):
-      def __init__(self, unique_id, model, pos, energy ,expertise_level ,fairness_level):
+class ScientistAgent(Agent):
+    """
+    Schelling segregation agent
+    """
 
-        super().__init__(unique_id, model)
-        self.pos = np.array(pos)
-        self.energy = energy
-        self.expertise_level = expertise_level
-        self.fairness_level = fairness_level
+    def __init__(self, pos, model, agent_type):
+        """
+        Create a new Schelling agent.
+        Args:
+           unique_id: Unique identifier for the agent.
+           x, y: Agent initial location.
+           agent_type: Indicator for the agent's type (minority=1, majority=0)
+        """
+        super().__init__(pos, model)
+        self.pos = pos
+        self.type = agent_type
 
+        # self.energy = energy
+        # self.expertise = expertise
+        # self.justice = justice
+        
 
+    def step(self):
+        similar = 0
+        for neighbor in self.model.grid.neighbor_iter(self.pos):
+            if neighbor.type == self.type:
+                similar += 1
+
+        # If unhappy, move:
+        if similar < self.model.homophily:
+            self.model.grid.move_to_empty(self)
+        else:
+            self.model.happy += 1
 
 
 class Environment(Model):
-      def __init__(self, N, height=100, width=100):
+    """
+    Model class for the Schelling segregation model.
+    """
 
-
-        self.num_agents = N
-
-        for i in range(self.num_agents):
-            a = Scientist(i, self)
-            self.schedule.add(a)
+    def __init__(self, height=50, width=50, density=0.8, minority_pc=0.2):
+        """"""
 
         self.height = height
         self.width = width
-
+        self.density = density
+        self.minority_pc = minority_pc
+        
 
         self.schedule = RandomActivation(self)
         self.grid = SingleGrid(width, height, torus=True)
 
+        
+        for cell in self.grid.coord_iter():
+            x = cell[1]
+            y = cell[2]
+            if self.random.random() < self.density:
+                if self.random.random() < self.minority_pc:
+                    agent_type = 1
+                else:
+                    agent_type = 0
 
-      # def step(self):
-      #   """
-      #   Run one step of the model. If All agents are happy, halt the model.
-      #   """
-      #   self.happy = 0  # Reset counter of happy agents
-      #   self.schedule.step()
-      #   # collect data
-      #   self.datacollector.collect(self)
+                agent = ScientistAgent((x, y), self, agent_type)
+                self.grid.position_agent(agent, (x, y))
+                self.schedule.add(agent)
 
-      #   if self.happy == self.schedule.get_agent_count():
-      #       self.running = False
+        self.running = True
+        # self.datacollector.collect(self)
+
+    def step(self):
+        """
+        Run one step of the model. If All agents are happy, halt the model.
+        """
+        self.happy = 0  # Reset counter of happy agents
+        self.schedule.step()
+        # collect data
+        self.datacollector.collect(self)
+
+        if self.happy == self.schedule.get_agent_count():
+            self.running = False
