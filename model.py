1ca7ad0da5cbcaca61788309aa2959bdfe1fde31
Polina <ptomasevskaa30@gmail.com>
2021-03-18 17:21:15 +0300

    commit

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
+import random
 
 class Scientist(Agent):
 
@@ -9,18 +10,25 @@ class Scientist(Agent):
         self.energy = energy
         self.justice = justice
         self.expertise = expertise
+        print("Energy", energy)
+        print("Justice", justice)
+        print("Expertise", expertise)
+    
 
 class Environment(Model):
 
     def __init__(self, N, width, height):
-        self.num_agents = N
+        self.num_agents = 10
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
         # Create agents
         for i in range(self.num_agents):
-            a = Scientist(i, self, energy=1, justice=2, expertise=3)
-            self.schedule.add(a)
+            a = Scientist(i,self, 
+            energy=random.normalvariate(10, 2), 
+            justice=random.normalvariate(10, 2), 
+            expertise=random.normalvariate(10, 2))
 
+            self.schedule.add(a)
             # Add the agent to a random grid cell
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
