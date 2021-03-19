4a8aa9c8195f6b15bd921c89adccef1e41c6a9af
Polina <ptomasevskaa30@gmail.com>
2021-03-19 18:09:53 +0300

    commit

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ from mesa import Agent, Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
 import random
+from mesa.datacollection import DataCollector
 
 class Scientist(Agent):
 
@@ -14,11 +15,25 @@ class Scientist(Agent):
         print("Justice", justice)
         print("Expertise", expertise)
 
+    def step(self):
+    
+        obj = Object(
+        self.next_id(),
+        )
+        self.grid.place_agent(obj, self.pos)
+        self.schedule.add(obj)
+
+
+        
+
 
 class Object(Agent):
 
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
+        
+
+    
  
         
         
@@ -29,7 +44,7 @@ class Environment(Model):
 
     def __init__(self, N, O, width, height):
         self.num_agents = 10
-        
+        self.running = True
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
 
@@ -48,6 +63,15 @@ class Environment(Model):
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
             self.grid.place_agent(b, (x, y))
+
+    def step(self):
+        self.schedule.step()
+
+
+
+
+ 
+
 
 
 
--- server.py
+++ server.py
@@ -2,18 +2,36 @@ from model import *
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 
+OBJECT_COLOR = "#FF66FF"
+SCIENTIST_COLOR = "#660066"
 
-def agent_portrayal(a):
-    port = {"Shape": "circle",
-                 "Filled": "false",
-                 "Layer": 0,
-                 "Color": "green",
-                 "r": 0.5}
-    return port
+def scientist_object_portrayal(agent):
+    if agent is None:
+        return
+
+    portrayal = {
+        "Shape": "circle",
+        "x": agent.pos[0],
+        "y": agent.pos[1],
+        "Filled": "true",
+    }
+
+    if isinstance(agent, Scientist):
+
+        portrayal["Color"] = SCIENTIST_COLOR
+        portrayal["r"] = 0.8
+        portrayal["Layer"] = 1
+
+    elif isinstance(agent, Object):
+        portrayal["Color"] = OBJECT_COLOR
+        portrayal["r"] = 0.5
+        portrayal["Layer"] = 1
+    return portrayal
 
 
 
-grid = CanvasGrid(agent_portrayal, 30, 30, 500, 500)
+
+grid = CanvasGrid(scientist_object_portrayal, 30, 30, 500, 500)
 server = ModularServer(Environment,
                        [grid],
                        "ScientistModel",
